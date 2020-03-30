3f801ffc01006cf0e8397f3f09d77b5f541447dd
sharath mallam <sharathkumar.mallam1@t-mobile.com>
2020-03-29 22:58:15 -0500

    edabit-mar292020: hello world

---
+++ src/main/java/StringStretched.java
@@ -0,0 +1,20 @@
+public class StringStretched {
+
+    /*
+    https://edabit.com/challenge/Cqemt6ySdA883Td2F
+    is a String Stretched?
+Write a function that returns true if the first string is the second string stretched, and false otherwise. A stretch is to repeat each character in a string the same number of times.
+
+Examples
+isStretched("pppaaannndddaaa", "panda") ➞ true
+
+isStretched("sssshhiipp", "ship") ➞ false
+
+isStretched("magnet", "magnet") ➞ true
+
+isStretched("magneto", "magnet") ➞ false
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
